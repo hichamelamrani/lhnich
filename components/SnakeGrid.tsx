248c1a3d5d9d40dd69c7cbe10ffec7b41e3507bd
hichamelamrani <139113583+hichamelamrani@users.noreply.github.com>
2024-02-11 17:59:55 +0000

    build grid

--- components/SnakeGrid.tsx
+++ components/SnakeGrid.tsx
@@ -2,11 +2,19 @@
 
 const GRID_SIZE: 20;
 
-type Point= {
+type Point = {
     x: number;
-    y: number ;
+    y: number;
 }
 
 export default function SnakeGrid() {
-    return <div>Com</div>
+    return <div className="grid grid-cols-20 grid-rows-20 border">
+        {Array.from({ length: GRID_SIZE }).map((_, y) => (
+            <div className="flex" key={y}>
+                {Array.from({ length: GRID_SIZE }).map((_, x) => (
+                    <div key={x} className="w-5 h-5 border border-x-white"></div>
+                ))}
+            </div>
+        ))}
+    </div>
 }
